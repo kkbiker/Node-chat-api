5442783da7e9c5ce07cd4f9954a3719198403091
髙山 健太 <nakayosikouyama.kenta@gmail.com>
2025-05-26 09:39:48 +0900

    mailformat

--- config/mail.js
+++ config/mail.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT),
-    secure: false,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
